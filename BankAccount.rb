7cc659852723e76f109eb5867b9e4b1b3b512b01
likwokbiu <li_bill@ymail.com>
2018-02-02 15:55:51 -0500

    first commit

---
+++ BankAccount.rb
@@ -0,0 +1,30 @@
+class BankAccount
+
+  def initialize(balance, interest_rate)
+    @balance = balance
+    @interest_rate = interest_rate
+  end
+
+  def balance
+    return @balance
+  end
+
+  def interest_rate
+    return @interest_rate
+  end
+
+  def deposit=(amount)
+    @balance += amount
+  end
+
+  def withdraw=(amount)
+    if @balance >= amount
+      @balance -= amount
+    end
+  end
+
+  def gain_interest
+    @balance += @balance * @interest_rate
+  end
+
+end
---
+++ main1.rb
@@ -0,0 +1,16 @@
+require './BankAccount.rb'
+
+bill = BankAccount.new(500, 0.03)
+puts "Balance: #{bill.balance}"
+
+bill.deposit = (75)
+puts "Balance: #{bill.balance}"
+
+bill.withdraw = (25)
+puts "Balance: #{bill.balance}"
+
+bill.withdraw = (2500)
+puts "Balance: #{bill.balance}"
+
+bill.gain_interest
+puts "Balance: #{bill.balance}"
